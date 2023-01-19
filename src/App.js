952786bd7cf2c9df78b417cb6e1be69d370ab5be
Vitaly <poroshenkoe@gmail.com>
2023-01-19 05:10:40 +0300

    add json, add map react

--- src/App.js
+++ src/App.js
@@ -1,61 +1,41 @@
 import './App.scss';
+import info from './assets/info.json';
 
 function App() {
+  const cards = info;
+
   return (
     <div className="container">
       <h1 className="title">Ты сегодня покормил кота?</h1>
       <section className="card-list">
-        <div className="card-item">
-          <div className="card">
-            <p className="upper-text"></p>
-            <p className="productname"></p>
-            <p className="with what"></p>
-            <p className="count-case"></p>
-            <p className="count-present"></p>
-            <img className="cat"></img>
-            <div className="circle">
-              <p className="count"></p>
-              <p className="weight"></p>
+        {cards.map((card, index) => {
+          return (
+            <div className="card-item">
+              <div className="card-background">
+                <div className="card">
+                  <div className="text">
+                    <p className="upper-text">{card.upper}</p>
+                    <p className="productname">{card.productname}</p>
+                    <p className="with">{card.with}</p>
+                    <p className="count-case">{card.countCase}</p>
+                  </div>
+                  <img
+                    className="image-cat"
+                    src={card.imageUrl}
+                    alt="cat"
+                  ></img>
+                  <div className="circle">
+                    <p className="count">{card.count}</p>
+                    <p className="weight">{card.weight}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="description">
+                <p className="description-text">{card.description}</p>
+              </div>
             </div>
-          </div>
-          <div className="description">
-            <p>Чего сидишь? Порадуй котэ, купи.</p>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="card">
-            <p className="upper-text"></p>
-            <p className="productname"></p>
-            <p className="with what"></p>
-            <p className="count-case"></p>
-            <p className="count-present"></p>
-            <img className="cat"></img>
-            <div className="circle">
-              <p className="count"></p>
-              <p className="weight"></p>
-            </div>
-          </div>
-          <div className="description">
-            <p>Чего сидишь? Порадуй котэ, купи.</p>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="card">
-            <p className="upper-text"></p>
-            <p className="productname"></p>
-            <p className="with what"></p>
-            <p className="count-case"></p>
-            <p className="count-present"></p>
-            <img className="cat"></img>
-            <div className="circle">
-              <p className="count"></p>
-              <p className="weight"></p>
-            </div>
-          </div>
-          <div className="description">
-            <p>Чего сидишь? Порадуй котэ, купи.</p>
-          </div>
-        </div>
+          );
+        })}
       </section>
     </div>
   );
